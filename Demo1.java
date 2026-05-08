6d9788cea537d05a0b3dd155a25ddefcdeead24f
SahilSri00 <sknsrivastavainfinix+gh@gmail.com>
2026-05-09 00:57:53 +0530

    test bot

--- Demo1.java
+++ Demo1.java
@@ -2,7 +2,7 @@ public class Demo1 {
     /*
      * Adds two ints.
      */
-    public int add(int a, int b) {
-        return a + b;
+    public int add(int a, int b,int c) {
+        return a + b - c;
     }
-}
+} 
